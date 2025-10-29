7f281fdee5d1d695bcb9f91d1461e8038c16bc09
Plovotok <z-sizinov@mail.ru>
2025-10-29 13:59:15 +0300

    predictive back changes

--- composeApp/src/androidMain/kotlin/com/example/testkmpapp/presentation/BackAnimation.android.kt
+++ composeApp/src/androidMain/kotlin/com/example/testkmpapp/presentation/BackAnimation.android.kt
@@ -1,12 +1,21 @@
 package com.example.testkmpapp.presentation
 
+import androidx.compose.animation.core.FiniteAnimationSpec
+import androidx.compose.animation.core.tween
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layout
 import com.arkivanov.decompose.ExperimentalDecomposeApi
 import com.arkivanov.decompose.extensions.compose.stack.animation.StackAnimation
+import com.arkivanov.decompose.extensions.compose.stack.animation.StackAnimator
 import com.arkivanov.decompose.extensions.compose.stack.animation.fade
+import com.arkivanov.decompose.extensions.compose.stack.animation.isFront
 import com.arkivanov.decompose.extensions.compose.stack.animation.plus
 import com.arkivanov.decompose.extensions.compose.stack.animation.predictiveback.predictiveBackAnimation
 import com.arkivanov.decompose.extensions.compose.stack.animation.slide
 import com.arkivanov.decompose.extensions.compose.stack.animation.stackAnimation
+import com.arkivanov.decompose.extensions.compose.stack.animation.stackAnimator
 import com.arkivanov.essenty.backhandler.BackHandler
 
 @OptIn(ExperimentalDecomposeApi::class)
@@ -16,7 +25,31 @@ actual fun <C : Any, T : Any> backAnimation(
 ): StackAnimation<C, T> {
     return predictiveBackAnimation(
         backHandler = backHandler,
-        fallbackAnimation = stackAnimation(fade() + slide()),
+        fallbackAnimation = stackAnimation(iosLikeSlide()),
         onBack = onBack
     )
 }
+
+private fun iosLikeSlide(animationSpec: FiniteAnimationSpec<Float> = tween()): StackAnimator =
+    stackAnimator(animationSpec = animationSpec) { factor, direction, content ->
+        content(
+            Modifier
+                .then(if (direction.isFront) Modifier else Modifier.fade(factor + 1F))
+                .offsetXFactor(factor = if (direction.isFront) factor else factor * 0.5F)
+        )
+    }
+
+private fun Modifier.fade(factor: Float) =
+    drawWithContent {
+        drawContent()
+        drawRect(color = Color(red = 0F, green = 0F, blue = 0F, alpha = (1F - factor) / 4F))
+    }
+
+private fun Modifier.offsetXFactor(factor: Float): Modifier =
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+
+        layout(placeable.width, placeable.height) {
+            placeable.placeRelative(x = (placeable.width.toFloat() * factor).toInt(), y = 0)
+        }
+    }
